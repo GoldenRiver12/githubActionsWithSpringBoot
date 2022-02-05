093bd0d1b44c3b912a09a6fc70ef644949d3b694
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-05 15:51:21 +0900

    [add] 初期コミット

---
+++ src/main/java/com/example/demo/GithubActionsWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsWithSpringBootApplication.class, args);
+	}
+
+}
