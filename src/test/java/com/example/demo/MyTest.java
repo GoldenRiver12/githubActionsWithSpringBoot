6d5cfbfc2e4d4e8956bcd9ec7f916ded6a5f8ac0
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-05 16:46:12 +0900

    [add] 最低限の画面・テストを作成

---
+++ src/test/java/com/example/demo/MyTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyTest {
+
+	@Test
+	void sum() {
+		assertEquals(2, 1+1);
+	}
+
+}
